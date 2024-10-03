0f3a2e10f068cc20bf74542c0b4dfd905f9ce02c
abdolhamidbarfi <abdolhamidbarfi@yahoo.com>
2024-10-03 13:56:16 +0330

    initial project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,111 @@
+"use client"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react"
+
+
+function Login() {
+
+    const {push} = useRouter()
+    const handleSubmit = async (event: any) => {
+        event.preventDefault()
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries())
+
+        try {
+            const res = await axios.post("http://localhost/fonixstore/wp-json/jwt-auth/v1/token", data)
+            console.log(res.status)
+            if (res.status === 200) {
+                localStorage.setItem("token", res?.data.token)
+                push("/")
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    return (
+        < >
+            {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 " dir="rtl">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        alt="Your Company"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        className="mx-auto h-10 w-auto"
+                    />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        ورود به حساب کاربری
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                نام کاربری
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    رمز عبور
+                                </label>
+                                <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        فراموشی رمز عبور
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    autoComplete="current-password"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                ورود
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?{' '}
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Start a 14 day free trial
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
